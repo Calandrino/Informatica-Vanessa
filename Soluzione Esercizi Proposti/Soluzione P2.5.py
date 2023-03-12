4e19ac39ba08783ec5fe748dd7d1542f2b346241
Calandrino <126302076+Calandrino@users.noreply.github.com>
2023-03-12 18:39:40 +0100

    Create Soluzione P2.5.py
    Ipotetica soluzione dell'Esercizio Proposto P2.5

---
+++ Soluzione Esercizi Proposti/Soluzione P2.5.py
@@ -0,0 +1,10 @@
+num1 = int(input("Inserire il primo numero: "))
+num2 = int(input("Inserire il secondo numero: "))
+
+print("Somma: ", num1 + num2)
+print("Differenza: ", num1 - num2)
+print("Prodotto: ", num1 * num2)
+print("Valore medio: ", (num1 + num2) / 2)
+print("Distanza: ", abs(num1 - num2))
+print("Valore massimo: ", max(num1, num2))
+print("Valore minimo: ", min(num1, num2))
